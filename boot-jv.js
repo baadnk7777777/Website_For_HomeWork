f5e8eaacb585082b15713d64b9222d93880f9d88
Meliodas <banktv76@gmail.com>
2021-02-03 00:58:03 +0700

    Version Bootstrap

---
+++ boot-jv.js
@@ -0,0 +1,27 @@
+function myFunction1() {
+    var x = document.getElementById("content-code-1");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
+  function myFunction2() {
+    var x = document.getElementById("content-code-2");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
+  function myFunction3() {
+    var x = document.getElementById("content-code-3");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
